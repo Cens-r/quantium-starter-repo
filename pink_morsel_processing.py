3036845407223f8e7cf7464068a6854c7b0195be
Censor <22554514+Cens-r@users.noreply.github.com>
2025-02-16 16:29:41 -0500

    Refactored for clarity and readability

--- pink_morsel_processing.py
+++ pink_morsel_processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 DATA_DIRECTORY = Path("data")
+OUTPUT_FILE_PATH = Path("pink_morsel_sales_data.csv")
 
 # Define the structure of the processed data
 output_data = pd.DataFrame({
@@ -10,18 +11,19 @@ output_data = pd.DataFrame({
     "region": []
 })
 
-# Accumulate data
+# Accumulating the input data
 csv_data = [pd.read_csv(csv_file) for csv_file in DATA_DIRECTORY.glob("*.csv")]
 input_data = pd.concat(csv_data)
 
+# Cleaning up the input data
 input_data["price"] = input_data["price"].str.replace("$", "").astype(float)
 input_data["quantity"] = input_data["quantity"].astype(float)
 
+# Populating the input data into the output dataframe
 output_data["sales"] = input_data["price"] * input_data["quantity"]
 output_data["sales"] = output_data["sales"].apply(lambda value: '{:.2f}'.format(value))
-
 output_data["date"] = input_data["date"]
 output_data["region"] = input_data["region"]
 
 # Save the processed data
-output_data.to_csv(Path.cwd()/"pink_morsel_sales_data.csv", index=False)
+output_data.to_csv(OUTPUT_FILE_PATH, index=False)
